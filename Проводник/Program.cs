7096ad69bc5f1df5142b3153e5bb33e65e5d82e1
Zol0tc0m <burmkirill@gmail.com>
2023-11-28 14:55:28 +0300

    я создал проводник и загружаю его.

---
+++ Проводник/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.IO;
+using Проводник;
+
+class Programa
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Clear();
+            DriveInfo[] drivesinfo = DriveInfo.GetDrives();
+            foreach (DriveInfo driveinfo in drivesinfo)
+            {
+                Console.WriteLine($"      Имя диска: {driveinfo.Name}  Всего места на диске: {driveinfo.TotalSize / (1024 * 1024 * 1024)}  Доступно места: {driveinfo.AvailableFreeSpace / (1024 * 1024 * 1024)} ГБ");
+            }
+            int select = Strelki.Show(0, 1);
+            if (select == 0)
+            {
+                ShowPapka(drivesinfo[select].Name);
+            }
+            else if (select == -1)
+            {
+                Main();
+            }
+        }
+        void ShowPapka(string s)
+        {
+            while (true)
+            {
+                Console.Clear();
+                string[] paths = Directory.GetDirectories(s);
+                string[] pathsFiles = Directory.GetFiles(s);
+
+                foreach (string path in paths)
+                {
+                    Console.WriteLine("  " + path);
+                }
+                foreach (string pathFiles in pathsFiles)
+                {
+                    DirectoryInfo Files = new DirectoryInfo(pathFiles);
+                    Console.WriteLine($"      {pathFiles}  {Files.CreationTime}  {Files.Extension}");
+                }
+                //выводятся файлы
+                int posission = Strelki.Show(0, paths.Length + pathsFiles.Length - 1);
+                if (posission == -1)
+                {
+                    return;
+                }
+                if (posission < paths.Length)
+                {
+                    ShowPapka(paths[posission]);
+                }
+                else
+                {
+                    Process.Start(new ProcessStartInfo { FileName = pathsFiles[posission - paths.Length], UseShellExecute = true });
+                    Console.WriteLine("Запустилось");
+                }
+            }
+        }
+    }
+}
---
+++ Проводник/Strelki.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Проводник
+{
+    internal class Strelki
+    {
+        public static int Show(int minimum, int maximum)
+        {
+            {
+                int posission = 0;
+                ConsoleKeyInfo key;
+
+                do
+                {
+                    Console.SetCursorPosition(0, posission);
+                    Console.WriteLine("->");
+
+                    key = Console.ReadKey();
+
+                    Console.SetCursorPosition(0, posission);
+                    Console.WriteLine("  ");
+
+                    if (key.Key == ConsoleKey.UpArrow && posission != minimum)
+                    {
+                        posission--;
+                        
+                    }
+                    else if (key.Key == ConsoleKey.DownArrow && posission != maximum)
+                    {
+                        posission++;
+                    }
+                    else if (key.Key == ConsoleKey.Escape)
+                    {
+                        return -1;
+                    }
+                    else if (key.Key == ConsoleKey.F1)
+                    {
+
+
+                    }
+
+                } while (key.Key != ConsoleKey.Enter);
+                return posission;
+            }
+        }
+    }
+}
